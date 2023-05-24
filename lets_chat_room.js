b4fdf9396321e613c0dc8fc64e18ece2cf8302f6
Tusti-Gupta <86391913+Tusti-Gupta@users.noreply.github.com>
2023-05-23 22:44:29 -0700

    Add files via upload

---
+++ lets_chat_room.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCknTQB84fcEuEHIUx3DbJSIJoG065g9IU",
+    authDomain: "kwitter-e0997.firebaseapp.com",
+    databaseURL: "https://kwitter-e0997-default-rtdb.firebaseio.com",
+    projectId: "kwitter-e0997",
+    storageBucket: "kwitter-e0997.appspot.com",
+    messagingSenderId: "508836944438",
+    appId: "1:508836944438:web:2a64eb6ce286283addd697"
+  };
+firebase.initializeApp(firebaseConfig);
