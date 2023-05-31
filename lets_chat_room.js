daf3dd3849452dfa0c7e706ef8130fa437ace43e
Tusti-Gupta <86391913+Tusti-Gupta@users.noreply.github.com>
2023-05-30 22:49:40 -0700

    Add files via upload

--- lets_chat_room.js
+++ lets_chat_room.js
@@ -8,3 +8,29 @@ const firebaseConfig = {
     appId: "1:508836944438:web:2a64eb6ce286283addd697"
   };
 firebase.initializeApp(firebaseConfig);
+
+function addRoom() {
+  room_name = document.getElementById("room_name").value;
+  firebase.database().ref("/").child(room_name).update({
+        purpose: "adding room"
+  });
+  localStorage.setItem("room_name", room_name);
+  window.location = "lets_chat_page.html";
+}
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+  Room_names=childKey;
+  console.log("Room Names - " + Room_names);
+  row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)'>#"+ Room_names + "</div>";
+  document.getElementById("output").innerHTML += row;
+});});}
+getData();
+
+user_name= localStorage.getItem("User_name");
+document.getElementById("user_name").innerHTML = user_name;
+
+function redirectToRoomName(name) {
+  console.log(name);
+  localStorage.setItem("room_name", name);
+  window.location = "lets_chat_page.html";
+}
